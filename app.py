04a9ffaa473ff90e8da27bfabd6885e0b1b4ce72
Bernardw11 <berniewang11@gmail.com>
2020-07-20 14:08:04 -0400

    yay gif thing done

--- app.py
+++ app.py
@@ -2,15 +2,25 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getImageUrlFrom
 import os
 
 # -- Initialization section --
 app = Flask(__name__)
 
+GIPHY_KEY = "xiiHwNzg3ajxC7yUqqsIB2HZWvUGpNpq"
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+@app.route('/yourgif', methods = ["GET", "POST"])
+def yourgif():
+    if request.method == "POST":
+        choice = request.form['gifchoice']
+        image = getImageUrlFrom(choice, GIPHY_KEY)
+        return render_template("yourgif.html", time = datetime.now(), image = image)
+    else:
+        return "bruh error."
--- model.py
+++ model.py
@@ -1,2 +1,6 @@
-def getImageUrlFrom(query):
-    pass
+import requests
+
+def getImageUrlFrom(query, key):
+    giphy_query = f"https://api.giphy.com/v1/gifs/search?api_key={key}&q={query}&limit=25&offset=0&rating=g&lang=en"
+    response = requests.get(giphy_query).json()
+    return response['data'][0]['images']['fixed_height']['url']
